11f9168d9b456d95819501264e49f7a525b87e4d
xiaolin99 <xiao.lin99@gmail.com>
2014-11-11 14:06:38 -0700

    Done!

--- src/main/java/cpsc441/solution/RID_Allocator.java
+++ src/main/java/cpsc441/solution/RID_Allocator.java
@@ -1,18 +1,22 @@
-/**
- * 
- */
 package cpsc441.solution;
+
+/**
+ * CPSC441 - Assignment3
+ * This class attemps to check if router id is unique
+ * by Xiao Lin
+ */
+
+import cpsc441.doNOTmodify.*;
 
 /**
  * @author Xiao
  *
  */
 public class RID_Allocator {
-	public final static int MAX_ROUTERS = 10;
-	private static boolean router_ids[] = new boolean[MAX_ROUTERS];
+	private static boolean router_ids[] = new boolean[DVRInfo.MAX_ROUTERS];
 	
 	public static boolean alloc_rid(int rid) {
-		if (rid >= MAX_ROUTERS || rid < 0) return false;
+		if (rid >= DVRInfo.MAX_ROUTERS || rid < 0) return false;
 		if (router_ids[rid]) return false;
 		else {
 			router_ids[rid] = true;
@@ -21,7 +25,7 @@ public class RID_Allocator {
 	}
 	
 	public static boolean dealloc_rid(int rid) {
-		if (rid >= MAX_ROUTERS || rid < 0) return false;
+		if (rid >= DVRInfo.MAX_ROUTERS || rid < 0) return false;
 		if (!router_ids[rid]) return false;
 		else {
 			router_ids[rid] = false;
--- src/main/java/cpsc441/solution/Router.java
+++ src/main/java/cpsc441/solution/Router.java
@@ -1,8 +1,14 @@
 package cpsc441.solution;
 
+/*
+ * CPSC441 - Assignment3 Router.java
+ * by Xiao Lin
+ */
+
 import java.io.*;
 import java.net.*;
 import java.util.Properties;
+
 import cpsc441.doNOTmodify.*;
 
 public class Router implements Runnable {
@@ -12,17 +18,19 @@ public class Router implements Runnable {
 	private int COST_INFTY;
 	private My_UDPSocket sock;
 	private int rid;
-	private int[] seqnum = new int[RID_Allocator.MAX_ROUTERS];
+	private int[] seqnum;
 	private int numNeighbors;
-	private int[] neighbors = new int[RID_Allocator.MAX_ROUTERS];
-	private boolean[] isNeighbor = new boolean[RID_Allocator.MAX_ROUTERS];
-	private int DV[][] = new int[RID_Allocator.MAX_ROUTERS][RID_Allocator.MAX_ROUTERS];
-	private int nexthops[] = new int[RID_Allocator.MAX_ROUTERS];
+	private int[] neighbors;
+	private boolean[] isNeighbor;
+	private int[][] DV;
+	private int[] nexthops;
 	private ILogFactory logFactory;
+	private int MAX_ROUTERS;
 	
 	private final int SEQNUM_MAX = 1000;
 	
     public Router(int rid, My_UDPSocket sock, ILogFactory logFactory) {
+    	// this code to read config.txt is copied directly from HelperUtils
     	try {
 			Properties properties = new Properties();
 			properties.load(new FileReader("config.txt"));
@@ -32,8 +40,9 @@ public class Router implements Runnable {
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
-    	
+    	this.MAX_ROUTERS = DVRInfo.MAX_ROUTERS;    	
     	this.sock = sock;
+    	// set timeout here
     	try {
 			sock.setSoTimeout(ARQ_TIMER);
 		} catch (SocketException e) {
@@ -41,6 +50,34 @@ public class Router implements Runnable {
 		}
     	this.rid = rid;
     	this.logFactory = logFactory;
+    }
+    
+    /**
+     * Functions to init various arrays of this router
+     */
+    private void initDV() {
+    	for (int i = 0; i < MAX_ROUTERS; i ++) {
+    		for (int j = 0; j < MAX_ROUTERS; j ++) DV[i][j] = COST_INFTY;
+    	}
+    }
+    private void initNeighbor() {
+    	numNeighbors = 0;
+    	for (int i = 0; i < MAX_ROUTERS; i ++) neighbors[i] = -1;
+    	for (int i = 0; i < MAX_ROUTERS; i ++) isNeighbor[i] = false;
+    }
+    private void initNextHop() {
+    	for (int i = 0; i < MAX_ROUTERS; i ++) nexthops[i] = -1;
+    }
+    private void initSeqnum() {
+    	for (int i = 0; i < MAX_ROUTERS; i ++) seqnum[i] = 0;
+    	seqnum[rid] = rid+100; // so each router will start with their own unique seqnum
+    }
+    private void initAll() {
+    	seqnum = new int[MAX_ROUTERS];
+    	neighbors = new int[MAX_ROUTERS];
+    	isNeighbor = new boolean[MAX_ROUTERS];
+    	DV = new int[MAX_ROUTERS][MAX_ROUTERS];
+    	nexthops = new int[MAX_ROUTERS];
     	initDV();
     	initNeighbor();
     	initNextHop();
@@ -48,30 +85,10 @@ public class Router implements Runnable {
     }
     
     /**
-     * Init various arrays of this router
-     */
-    private void initDV() {
-    	for (int i = 0; i < RID_Allocator.MAX_ROUTERS; i ++) {
-    		for (int j = 0; j < RID_Allocator.MAX_ROUTERS; j ++) DV[i][j] = COST_INFTY;
-    	}
-    }
-    private void initNeighbor() {
-    	numNeighbors = 0;
-    	for (int i = 0; i < RID_Allocator.MAX_ROUTERS; i ++) neighbors[i] = -1;
-    	for (int i = 0; i < RID_Allocator.MAX_ROUTERS; i ++) isNeighbor[i] = false;
-    }
-    private void initNextHop() {
-    	for (int i = 0; i < RID_Allocator.MAX_ROUTERS; i ++) nexthops[i] = -1;
-    }
-    private void initSeqnum() {
-    	for (int i = 0; i < RID_Allocator.MAX_ROUTERS; i ++) seqnum[i] = 0;
-    }
-    
-    /**
      * increment seqnum, warp when reached SEQNUM_MAX
      */
     private void incrementSeqnum() {
-    	for (int i = 0; i < RID_Allocator.MAX_ROUTERS; i ++) {
+    	for (int i = 0; i < MAX_ROUTERS; i ++) {
     		seqnum[i]++;
     		if (seqnum[i] > SEQNUM_MAX) seqnum[i] = 0;
     	}
@@ -84,12 +101,12 @@ public class Router implements Runnable {
     private boolean recalculateDV()  {
     	boolean changed = false;
     	// rid -> current router, y -> destination, neighbors[v] -> neighbor
-    	for (int y = 0; y < RID_Allocator.MAX_ROUTERS; y ++) {
+    	for (int y = 0; y < MAX_ROUTERS; y ++) {
     		for (int v = 0; v < numNeighbors; v ++){
-    			int DV_x_to_y = DV[rid][neighbors[v]]+DV[neighbors[v]][y];
-    			if (DV_x_to_y > COST_INFTY) DV_x_to_y = COST_INFTY;
-    			if (DV[rid][y] > DV_x_to_y){
-    				DV[rid][y] = DV_x_to_y;
+    			int dvXY = DV[rid][neighbors[v]] + DV[neighbors[v]][y];
+    			if (dvXY > COST_INFTY) dvXY = COST_INFTY;
+    			if (DV[rid][y] > dvXY){
+    				DV[rid][y] = dvXY;
     				nexthops[y] = neighbors[v];
     				changed = true;
     			}
@@ -103,7 +120,7 @@ public class Router implements Runnable {
      */
     private void sendDVToNeighbors(PrintWriter log) {
     	for (int v = 0; v < numNeighbors; v ++){
-    		DVRInfo snd_packet = new DVRInfo(rid, neighbors[v], seqnum[neighbors[v]], DVRInfo.PKT_ROUTE);
+    		DVRInfo snd_packet = new DVRInfo(rid, neighbors[v], seqnum[rid], DVRInfo.PKT_ROUTE);
 			snd_packet.mincost = DV[rid];
 			try {
 				if (log != null) log.printf("[%d] send %s]\n", rid, snd_packet);
@@ -127,7 +144,11 @@ public class Router implements Runnable {
     }
     
 
+    /**
+     * Router running ...
+     */
     public void run() {
+    	// setup log
     	PrintStream logStream = null;
     	PrintWriter log = null;
     	try {
@@ -156,7 +177,8 @@ public class Router implements Runnable {
     					&& rcv_packet.destid == rid 
     					&& rcv_packet.type == DVRInfo.PKT_ROUTE) success = true;
     		} catch (SocketTimeoutException e1) {
-    			System.out.println("socket timeout");
+    			// UDP reliablility (timeout)
+    			System.out.println("NEM unresponsive!");
     			continue;
     		} catch (IOException e) {
     			System.out.println("failed to send hello packet.");
@@ -165,15 +187,19 @@ public class Router implements Runnable {
     		} 
     	}
     	
+    	// setup neighbor info and initialize all arrays
+    	MAX_ROUTERS = rcv_packet.mincost.length;
+    	initAll();
+    	DV[rid] = rcv_packet.mincost;
 		for (int i = 0; i < rcv_packet.mincost.length; i ++) {
 			if (rcv_packet.mincost[i] < COST_INFTY) {
 				neighbors[numNeighbors] = i;
 				numNeighbors ++;
 				isNeighbor[i] = true;
 				nexthops[i] = i;
-				DV[rid][i] = rcv_packet.mincost[i];
 			}
 		}
+		System.out.println("Neighbors: "+numNeighbors);
 		
 		// send DV to neighbors
 		this.sendDVToNeighbors(log);
@@ -183,16 +209,20 @@ public class Router implements Runnable {
 				rcv_packet = sock.receive();
 				// check if packet is intended for this router
 				if (rcv_packet.destid == rid) {
-					if (log != null) log.printf("[%d] receive %s]\n", rid, rcv_packet);
+					// quit if received PKT_QUIT
 					if (rcv_packet.type == DVRInfo.PKT_QUIT) break;
 					if (rcv_packet.sourceid == NEM_ID) continue;
 					if (isNeighbor[rcv_packet.sourceid] && rcv_packet.type == DVRInfo.PKT_ROUTE) {
+						// BONUS: check seqnum, if unchanged, skip loop
 						if (rcv_packet.seqnum == seqnum[rcv_packet.sourceid]) continue;
 						else seqnum[rcv_packet.sourceid] = rcv_packet.seqnum;
+						System.out.println("Neighbour's DV updated!");
+						if (log != null) log.printf("[%d] receive %s]\n", rid, rcv_packet);
 						// calculate DV and send to neighbors if DV changed
 						DV[rcv_packet.sourceid] = rcv_packet.mincost;
 						if (this.recalculateDV()) {
 							// increment seqnum if DV changed, then sent to neighbours
+							System.out.println("DV changed!");
 							incrementSeqnum();
 							this.sendDVToNeighbors(log);
 						}
